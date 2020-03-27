7dcfde85c642f32496e5de24c855af1c9f609729
dx <1289179220@qq.com>
2020-03-27 15:47:08 +0800

    1

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,43 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <el-form ref="formInline" :inline="true" :model="formInline" class="demo-form-inline">
+      <el-form-item label="demo1" prop="user">
+        <el-input v-model="formInline.user" placeholder="请输入" />
+      </el-form-item>
+      <el-form-item label="demo2" prop="content">
+        <el-input v-model="formInline.content" placeholder="请输入" />
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="onSubmit('formInline')">查询</el-button>
+      </el-form-item>
+    </el-form>
   </div>
 </template>
-
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      formInline: {
+        user: '',
+        content: ''
+      }
+    }
+  },
+  methods: {
+    onSubmit(formName) {
+      this.$refs[formName].validate((valid) => {
+        if (valid) {
+          alert('submit!')
+        } else {
+          console.log('error submit!!')
+          return false
+        }
+      })
+    }
   }
+
 }
 </script>
