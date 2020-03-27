e3efe18ad267b63c09ce4104a8587cda74ecad9c
dx <1289179220@qq.com>
2020-03-27 17:10:45 +0800

    动态添加表单

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 Vue.use(ElementUI, { size: 'medium', zIndex: 3000 })
 
 Vue.config.productionTip = false
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,22 @@
 <template>
-  <div class="home">
+  <div>
     <el-form ref="formInline" :inline="true" :model="formInline" class="demo-form-inline">
-      <el-form-item label="demo1" prop="user">
-        <el-input v-model="formInline.user" placeholder="请输入" />
-      </el-form-item>
-      <el-form-item label="demo2" prop="content">
-        <el-input v-model="formInline.content" placeholder="请输入" />
-      </el-form-item>
-      <el-form-item>
-        <el-button type="primary" @click="onSubmit('formInline')">查询</el-button>
-      </el-form-item>
+      <div v-for="(v,i) in formInline.formList" :key="i">
+        <el-form-item label="demo1" :prop="'formList.'+i+'.user'" :rules="{ required: true,message: '必填字段不能为空'}">
+          <el-input v-model="v.user" placeholder="请输入" />
+        </el-form-item>
+        <el-form-item label="demo2" :prop="'formList.'+i+'.phone'" :rules="[{ required: true,message: '必填字段不能为空'},{ pattern:/^1(3|4|5|7|8)\d{9}$/, message: '请输入正确的手机号', trigger: 'change' }]">
+          <el-input v-model="v.phone" placeholder="请输入" />
+        </el-form-item>
+        <el-form-item>
+          <el-button v-if="i===0" type="primary" @click="add()">添加</el-button>
+          <el-button v-else type="danger" @click="dels(v,i)">删除</el-button>
+        </el-form-item>
+      </div>
     </el-form>
+    <div>
+      <el-button type="primary" @click="onSubmit('formInline')">提交</el-button>
+    </div>
   </div>
 </template>
 <script>
@@ -21,12 +27,27 @@ export default {
   data() {
     return {
       formInline: {
-        user: '',
-        content: ''
+        formList: [
+          {
+            user: '',
+            phone: ''
+          }
+        ]
       }
     }
   },
   methods: {
+    add() {
+      this.formInline.formList.push({
+        user: '',
+        phone: ''
+      })
+    },
+    dels(v, i) {
+      if (this.formInline.formList.indexOf(v) > -1) {
+        this.formInline.formList.splice(i, 1)
+      }
+    },
     onSubmit(formName) {
       this.$refs[formName].validate((valid) => {
         if (valid) {
